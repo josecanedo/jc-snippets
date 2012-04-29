f357814c6ae408cad8c6fd2ce52b9a9c4d57eb21
jose.canedo@gmail.com <jose.canedo@gmail.com@3710a10a-ec7b-2986-0bc0-1d7eb069636b>
2012-04-29 22:01:39 +0000

    std::vector

---
+++ trunk/std-vector.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+using namespace std;
+
+/*initialization*/
+//vector<T> tvec(10); //create vector of 10 elements of type T
+vector<int> vec0; empty vector
+vector<int> vec1(10, 42); //10 elements init to 42
+vector<int> vec3(vec1); //vec3 is a copy of vec1
+
+/*access*/
+for(int i = 0; i < vec.size(); ++i){vec[i] = i;}
+for(vec<int>::iterator i = vec.begin(); i  != vec.end(); ++i){*i = 42;}
+for(auto i = vec.begin(); i  != vec.end(); ++i){*i = 42;}
+
+/*algorithms*/
+sort(vec.begin(), vec.end());
+T found = find(vec.begin(), vec.end()); //if not found, found = vec.end()
+reverse(vec.begin(), vec.end());
